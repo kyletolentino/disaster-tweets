13e586a905f4e4bcb57e8371bf81bea3921ba486
Kyle Tolentino <kyltol@live.com>
2020-01-02 00:16:00 -0600

    First commit, basic code to analyze data and split for training and testing

--- disaster.py
+++ disaster.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-
+from nltk import TweetTokenizer, RegexpTokenizer
 # import tensorflow as tf
 
 df = pd.read_csv('datasets/train.csv')
@@ -18,4 +18,15 @@ plt.title('Tweets about Disasters Distribution \n (0: Normal // 1: Disaster)')
 X = df.drop(['target', 'id'], axis=1)
 y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# tokenize the text (separate into words)
+tw_tk = TweetTokenizer(strip_handles=True, reduce_len=True)
+# .loc is used to make sure we work on original dataframe, ignore warning message
+X_train.loc[:, 'text'] = X_train['text'].apply(tw_tk.tokenize)
+print(X_train['text'].head())
+
+# clean up the text by removing punctuation and stop-words, and converting all words to lower case
+
+
+
 
--- disaster_text_processing.py
+++ disaster_text_processing.py
@@ -1,28 +1,19 @@
 import pandas as pd
 from nltk.tokenize import sent_tokenize, word_tokenize, TweetTokenizer
-from nltk.stem import SnowballStemmer
+from string import punctuation
 import re
 
-snowball = SnowballStemmer(language='english')
+
+def preprocessing(list):
+    new_list = []
+    for i in list:
+        if i.isalpha():
+            new_list.append(i)
+    return new_list
 
 
-def preprocess(text):
-    clean = []
-    for i in (text[:][0]):
-        new_text = re.sub('<.*?>', '', i)  # remove HTML tags
-        new_text = re.sub(r'[^\w\s]', '', new_text)  # remove punctuation
-        new_text = re.sub(r'\d+', '', new_text)  # remove numbers
-        new_text = new_text.lower()  # make everything lowercase
-        if new_text != '':
-            clean.append(new_text)
-    return clean
-
-
-def stemming(words):
-    stem_new = []
-    for i in (words[:][0]):
-        stemmed = snowball.stem(i)
-        stem_new.append(stemmed)
-    return stem_new
-
-
+tk = TweetTokenizer(strip_handles=True, reduce_len=True)
+s = " What's really good @kyletolentino ? >:)    "
+s2 = tk.tokenize(s)
+s3 = preprocessing(s2)
+print(s3)
