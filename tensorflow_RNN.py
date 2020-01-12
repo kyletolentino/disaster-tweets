872a82c6ad4b17a9b953d86767a1f097bed6df52
Kyle Tolentino <kyltol@live.com>
2020-01-12 02:34:00 -0600

    Enabled drop_remainder for batches (so no uneven), found batch_size was best at 32, removed test_df, added Dropout (set to 0.4), highest val_accuracy was 0.7999

--- tensorflow_RNN.py
+++ tensorflow_RNN.py
@@ -2,10 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-
 import tensorflow as tf
-from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras import layers
 
 df = pd.read_csv('../datasets/train.csv')
 
@@ -16,16 +15,17 @@ df = pd.read_csv('../datasets/train.csv')
 # plt.show()
 # ~ 40/60 real-to-normal split
 
-train_df, test_df = train_test_split(df, test_size=0.2)
-train_df, val_df = train_test_split(train_df, test_size=0.2)
-# 4872/1523/1218 - train/test/val length
+# train_df, test_df = train_test_split(df, test_size=0.2, random_state=1)
+# train_df, val_df = train_test_split(train_df, test_size=0.25, random_state=1)
+# 60/20/20 - 0.2, 0.25
+
+train_df, val_df = train_test_split(df, test_size=0.2, random_state=1)
+# new approach: only focus on train/val data -> once I decide on the model, then use test.csv
 
 # tokenize and update vocabulary based on training text
-tk = Tokenizer(oov_token='<unknown>')
+tk = Tokenizer()
 train_text = train_df['text'].to_numpy()
 tk.fit_on_texts(train_text)
-tk.word_index['<pad>'] = 0
-tk.index_word[0] = '<pad>'
 
 vocab_size = len(tk.word_index) + 1
 
@@ -49,28 +49,32 @@ train_seq = text_to_seq(train_text)
 train_target = train_df['target'].to_numpy()
 
 val_ds = df_to_ds(val_df)
-test_ds = df_to_ds(test_df)
+# test_ds = df_to_ds(test_df)
 
 # creating datasets
-BUFFER_SIZE = 1000
-BATCH_SIZE = 50
+BUFFER_SIZE = 10000
+BATCH_SIZE = 32
+# max batch size = available GPU memory bytes / 4 / (size of tensors + trainable parameters)
+# 2108 MB memory
 
 train_ds = tf.data.Dataset.from_tensor_slices((train_seq, train_target))
-train_ds = train_ds.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
-val_ds = val_ds.batch(BATCH_SIZE)
-test_ds = test_ds.batch(BATCH_SIZE)
+train_ds = train_ds.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+val_ds = val_ds.batch(BATCH_SIZE, drop_remainder=True)
+# test_ds = test_ds.batch(BATCH_SIZE, drop_remainder=True)
+
 
 # prefetching is used to decouple the time between when data is produced and consumed
 train_ds = train_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 val_ds = val_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-test_ds = test_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+# test_ds = test_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
 # buld a sequential model for RNN
 model = tf.keras.Sequential([
     layers.Embedding(vocab_size, 64),
     layers.Bidirectional(tf.keras.layers.LSTM(64)),
-    layers.Dense(64, activation='relu'),
+    layers.Dense(32, activation='relu'),
+    layers.Dropout(0.4),
     layers.Dense(1, activation='sigmoid')
 ])
 
@@ -78,12 +82,14 @@ model.compile(loss='binary_crossentropy',
               optimizer=tf.keras.optimizers.Adam(1e-4),
               metrics=['accuracy'])
 
-history = model.fit(train_ds, epochs=50, validation_data=val_ds)
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
 
-test_loss, test_acc = model.evaluate(test_ds)
-print('\n')
-print('Test Loss: {}'.format(test_loss))
-print('Test Accuracy: {}'.format(test_acc))
+history = model.fit(train_ds, epochs=50, validation_data=val_ds, callbacks=[callback])
+
+# test_loss, test_acc = model.evaluate(test_ds)
+# print('\n')
+# print('Test Loss: {}'.format(test_loss))
+# print('Test Accuracy: {}'.format(test_acc))
 
 
 def plot_graphs(history, string):
@@ -97,6 +103,3 @@ def plot_graphs(history, string):
 
 plot_graphs(history, 'accuracy')
 plot_graphs(history, 'loss')
-
-
-
